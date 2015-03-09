90c9c219252717fb1cf41f929fb2e057639dda7f
Marcelo Almeida <marcelopazzo@gmail.com>
2015-03-09 03:01:09 -0300

    Add associations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  has_many :points
+
+  def paths
+    points.flat_map &:paths
+  end
 
 end
--- app/models/point.rb
+++ app/models/point.rb
@@ -2,6 +2,7 @@ class Point < ActiveRecord::Base
   belongs_to :location
   validates :name, presence: true
   validates :name, uniqueness: { scope: :location }
+  has_many :paths, foreign_key: "point1_id"
 
   scope :by_location, ->(location_id) { where(:location_id => location_id)}
 end
